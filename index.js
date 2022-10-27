a75f9f1b459bd93cf0afa6b555af3522fe36d76b
Phoebe Orwell <phoebe.orwell@gmail.com>
2022-10-27 14:15:59 +0200

    Iteration2

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 //Make sure to require your model so you can connect to the DB
 //This line of code connects to the Database
-require("./db");
+const {Schema, model} = require("./db");
 
 //Iteration 2:
 //create a user as an object
+const vikingSchema = new Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    familyName: {
+        type: String,
+        required: true
+    },
+    strength: Number,
+    weapon: String
+})
+
+const vikingModel = model("viking", vikingSchema);
+
+model.exports = vikingModel;
 
 //Iteration 3:
 //Insert the new user that you created into the Database
